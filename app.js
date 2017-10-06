b79de56c70d3e51d2981a37acf32c7071f8f8cd2
Caleb Cox <caleb@caleb-cox.com>
2017-10-06 13:01:46 -0400

    Removed mongoose to avoid Schema model issue

--- app.js
+++ app.js
@@ -1,116 +1,94 @@
 // setup app and requires
-var express = require('express'),
+const express = require('express'),
     ejs = require('ejs'),
     app = express(),
     validUrl = require('valid-url'),
     shortId = require('shortid'),
-    mongoose = require('mongoose'),
+    mongodb = require('mongodb'),
+    MongoClient = mongodb.MongoClient,
     url = process.env.MONGOLAB_URI;
 
 // set views to render html
 app.set("view engine", "ejs");
 
-var urlSchema = new mongoose.Schema({
-    original: String,
-    minified: String
-});
-
-// // setup schema
-// var urlSchema = new mongoose.Schema({
-//     original: String,
-//     minified: String
-// });
-
-// var Entry = mongoose.model("Entry", urlSchema);
-
-// Test create
-// Entry.create({
-//     original: "http://google.com",
-//     minified: "http://g.com"
-// });
 
 app.get("/", function(req, res) {
     res.render("index");
 });
 
-app.get("/new/:url(*)", function(req, res) {
+app.get("/new/:url(*)", function(req, res, next) {
+    var host = req.get('host') + "/";
     var userInput = req.params.url;
 
     // connect to mLab DB
-    mongoose.connect(process.env.MONGOLAB_URI, {useMongoClient: true}, function(err, db) {
+    MongoClient.connect(process.env.MONGOLAB_URI, function(err, db) {
         if (err) {
             console.log("There was an error connecting to the database..");
         } else {
             console.log("Connected to database!");
-        }
+
+            var collection = db.collection('entries');
+
+            var newEntry = function(db, callback) {
+                collection.findOne({ "original": userInput }, { minified: 1, _id: 0 }, function(err, doc) {
+                    if (doc != null) {
+                        res.json({
+                            original: userInput,
+                            minified: host + doc.minified
+                        });
+                    } else {
+                        if (validUrl.isUri(userInput)) {
+                            var minified =  shortId.generate();
+                            var entry = { original: userInput, minified: minified };
+                            collection.insert([entry]);
+                            console.log("added new entry!");
+                            res.json({
+                                original: userInput,
+                                minified: host + minified
+                            });
+                        } else {
+                            res.json({ error: "Incorrect URL Format"});
+                        };
+                    };
+                });
+            };
+
+            newEntry(db, function() {
+                db.close();
+            });
+        };
     });
-
-    // setup schema
-    // var urlSchema = new mongoose.Schema({
-    //     original: String,
-    //     minified: String
-    // });
-    var schema = urlSchema;
-
-    var Entry = mongoose.model("Entry", schema);
-
-    if (validUrl.isUri(userInput)) {
-        var minified = shortId.generate();
-
-        Entry.create({
-            original: userInput,
-            minified: "localhost:3001/" + minified
-        });
-
-        console.log("localhost:3001/" + minified);
-        res.json({
-            original: userInput,
-            minified: "localhost:3001/" + minified
-        });
-        
-        mongoose.connection.close( function() {
-            console.log("DB Connection closed!");
-        });
-    } else {
-        res.json({
-            error: "Invalid URL was entered"
-        });
-    };
 });
 
 app.get("/:minified", function(req, res) {
-    mongoose.connect(process.env.MONGOLAB_URI, {useMongoClient: true}, function(err, db) {
+
+    var userInput = req.params.minified;
+
+    MongoClient.connect(process.env.MONGOLAB_URI, function (err, db) {
         if (err) {
-            console.log("There was an error connecting to the database");
+            console.log("There was an error connecting to the database...");
         } else {
             console.log("Connected to database!");
-        }
-    });
 
-    // This local variable declaration is broke, outputs as undefined [object Object].
-    var schema = urlSchema;
+            var collection = db.collection('entries');
 
-    console.log("Schema: " + schema);
+            var findEntry = function (db, callback) {
+                collection.findOne({ "minified": userInput}, {original: 1, _id: 0}, function (err, doc) {
+                    if (doc != null) {
+                        res.redirect(doc.original);
+                    } else {
+                        res.json({ err: "No such link found!" });
+                    };
+                });
+            };
 
-    var Entry = mongoose.model("Entry", schema);
-
-    Entry.findOne({ "minified": "localhost:3001/" + req.params.minified }, function(err, doc) {
-
-        console.log(req.params.minified);
-
-        if (doc != null) {
-            res.redirect(doc.url);
-            console.log("Link: " + doc);
-        } else {
-            res.json({ error: "No such link found" });
-        }
-    });    
-
-    mongoose.connection.close( function() {
-        console.log("DB Connection closed!");
+            findEntry(db, function() {
+                db.close();
+            });
+        };
     });
 });
 
-app.listen(3001, function() {
-    console.log("Server listening on port 3001...");
+app.listen(process.env.PORT, function() {
+    console.log("Server listening on port " + process.env.PORT + "...");
 });
